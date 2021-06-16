755c7c747a0004f72a66e712ebb6fe97d1a338bb
github username <github email address>
2021-06-15 21:29:55 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('%c HI', 'color: firebrick')
+const init = () => {
+    const inputForm = document.querySelector('label')
+    inputForm.addEventListener("selected", (event) => {
+        event.preventDefault()
+    
+    const selection = document.querySelector('select#breed-dropdown#option#value')
+
+
+    fetch(`https://dog.ceo/api/breeds/image/random/4`)
+    .then(response => response.json())
+    .then(data => {
+        const dogBreeds = document.querySelector('ul#dog-breeds')
+
+        if (selection === data.breeds[0])
+        {dogBreeds.innerText = data.breeds}
+    })
+    })
+}
+
+document.addEventListener('DOMContentLoaded', init);
